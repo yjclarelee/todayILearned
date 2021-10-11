4eb833cbcf26fd134611aa3143b92f6b171218f7
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-10-11 11:07:16 +0900

    Create basic modal

--- src/components/TILCard.jsx
+++ src/components/TILCard.jsx
@@ -8,7 +8,7 @@ const TILCard = (props) => {
 
   return <>
       <Card onClick={showModal} className="m-3">
-        <Card.Img src="https://images.unsplash.com/photo-1527427337751-fdca2f128ce5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"/>
+        <Card.Img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1471&q=80"/>
         <div className="p-2">
           <Card.Title>{props.title}</Card.Title>
           <Card.Text>{props.body}</Card.Text>
@@ -20,7 +20,7 @@ const TILCard = (props) => {
         <Modal.Title>{props.title}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <img className="w-100" src="https://images.unsplash.com/photo-1527427337751-fdca2f128ce5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="content"/>
+        <img className="w-100" src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1471&q=80" alt="content"/>
         <div>{props.body}</div>
       </Modal.Body>
       <Modal.Footer>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,15 @@
 import React, {useEffect, useState} from 'react';
-import Navbar from "../components/Nav";
+import {Button, Form, Modal} from "react-bootstrap";
+import Nav from "../components/Nav";
 import TILCard from '../components/TILCard';
 import Footer from "../components/Footer";
 import axios from "axios";
 
 const Main = () => {
   const [cardData, setCardData] = useState([]);
+  const [isModal, setIsModal] = useState(false);
+  const showModal = () => setIsModal(true);
+  const hideModal = () => setIsModal(false);
 
   const getData = async () => {
     const res = await axios.get("http://localhost:4000/posts");
@@ -17,7 +21,7 @@ const Main = () => {
   }, [])
 
   return <div>
-    <Navbar></Navbar>
+    <Nav showModal={showModal}/>
     <div style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", marginLeft: "3.5rem", marginRight: "3.5rem"}}>
     {
       cardData.map(card => 
@@ -28,6 +32,26 @@ const Main = () => {
         />)
     }
     </div>
+    <Modal show={isModal} onHide={hideModal} dialogClassName="modal-90w modal-60v">
+      <Modal.Header closeButton>
+        <Modal.Title>새 글</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="title">
+            <Form.Label>제목</Form.Label>
+            <Form.Control type="text"/>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="body">
+            <Form.Label>내용</Form.Label>
+            <Form.Control as="textarea" rows={5}/>
+          </Form.Group>
+          <Button variant="outline-dark" type="submit">
+            글 작성
+          </Button>
+      </Form>
+      </Modal.Body>
+    </Modal>
     <Footer></Footer>
   </div>
 }
