1473609f9e0831495399e093eca7a6f3fb748abf
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-10-11 10:15:27 +0900

    Add margin and padding

--- src/components/TILCard.jsx
+++ src/components/TILCard.jsx
@@ -1,18 +1,16 @@
-import React, {useEffect} from "react";
+import React, {useState} from "react";
 import {Button, Card, Modal} from "react-bootstrap";
 
 const TILCard = (props) => {
-  const [isModal, setIsModal] = useEffect(false);
+  const [isModal, setIsModal] = useState(false);
   const showModal = () => setIsModal(true);
   const hideModal = () => setIsModal(false);
 
   return <>
-    <Button onClick={showModal}>
-      <Card>
+      <Card className="m-4 p-3">
         <Card.Title>{props.title}</Card.Title>
         <Card.Text>{props.body}</Card.Text>
       </Card>
-    </Button>
 
     <Modal show={isModal} onHide={hideModal}>
       <Modal.Header closeButton>
@@ -20,10 +18,10 @@ const TILCard = (props) => {
       </Modal.Header>
       <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
+        <Button variant="secondary" onClick={hideModal}>
           Close
         </Button>
-        <Button variant="primary" onClick={handleClose}>
+        <Button variant="primary" onClick={hideModal}>
           Save Changes
         </Button>
       </Modal.Footer>
